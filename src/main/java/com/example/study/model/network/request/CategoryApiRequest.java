f40511e3067f8cd08fd8e2e204c3b8a7c5402d56
cheogodzip <lkver@naver.com>
2021-04-05 17:45:16 +0900

    Category API 작성

---
+++ src/main/java/com/example/study/model/network/request/CategoryApiRequest.java
@@ -0,0 +1,21 @@
+package com.example.study.model.network.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryApiRequest {
+
+    private Long id;
+
+    private String type;
+
+    private String title;
+}
